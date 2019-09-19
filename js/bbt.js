c4db2d68f8213e3ddd4c7868923b7aa65cd8cb3a
Nayan Birnbach <nayanbirnbach@seattleacademy.org>
2019-09-19 13:25:53 -0700

    added twins but kinda worked trying to fix for most of time

--- js/bbt.js
+++ js/bbt.js
@@ -1,4 +1,4 @@
-const classes = ['amy', 'bernadette', 'howard', 'leonard', 'penny', 'raj', 'sheldon', 'stuart']
+const classes = ['Nayan', 'Zubin', 'amy', 'bernadette', 'howard', 'leonard', 'penny', 'raj', 'sheldon', 'stuart']
 
 function getFaceImageUri(className, idx) {
   return `images/${className}/${className}${idx}.png`
